84e455ca7e027421347b9e82e065fea74436874b
Divyam Patel <70924790+divyam-p@users.noreply.github.com>
2021-01-13 21:05:52 -0500

    Edited Slideshow Images.

--- frontend/eternity/src/home/SlideShow.js
+++ frontend/eternity/src/home/SlideShow.js
@@ -1,13 +1,8 @@
-// import image1 from './assets/slideShow/1.jpg'
-// import image2 from './assets/slideShow/2.jpg'
-// import image3 from './assets/slideShow/3.jpg'
 import './SlideShow.css'
 import React from 'react'
 import { useState, useEffect, useCallback} from 'react'
 import { IoIosArrowBack } from 'react-icons/io'
 import { IoIosArrowForward } from 'react-icons/io'
-
-//https://scrimba.com/scrim/cpqd9rta
 
 function SlideShow({slides}){  
     const [current, setCurrent] = useState(0); 
@@ -49,8 +44,8 @@ function SlideShow({slides}){
                             <IoIosArrowBack  onClick={() => {goToBack(); setCheck(false)}} id="arrowLeft"/>
                         </div>
                         <div>
-                            <h1>{s.title}</h1>
-                            <h2>{s.subtitle}</h2>
+                            <h1 className="slideshowProductInfo">{s.title}</h1>
+                            <h2 className="slideshowProductInfo">{s.subtitle}</h2>
                         </div>
                         <div>
                             <IoIosArrowForward onClick={() => {goToNext(); setCheck(false)}} id="arrowRight"/> 
@@ -66,6 +61,4 @@ function SlideShow({slides}){
     );
 }
 
-//IoIosArrowForward
-//IoIosArrowBack
 export default SlideShow
--- frontend/eternity/src/home/defaultSlides.js
+++ frontend/eternity/src/home/defaultSlides.js
@@ -1,24 +1,18 @@
 import image1 from '../assets/slideShow/1.jpg'
 import image2 from '../assets/slideShow/2.jpg'
-import image3 from '../assets/slideShow/3.jpg'
 
 
 const listOfImages = [
     {
-        title: 'First Slide', 
-        subtitle: 'is it working', 
+        title: 'Pop Socket', 
+        subtitle: 'Mount onto any phone!', 
         image: image1,
     },
     {
-        title: 'Second Slide', 
-        subtitle: 'is it working', 
+        title: 'Iphone Phone Case', 
+        subtitle: 'Cheap and Good Quality!', 
         image: image2,
-    },
-    {
-        title: 'Third Slide', 
-        subtitle: 'is it working', 
-        image: image3,
-    },
+    }
 ]
 
 export default listOfImages
